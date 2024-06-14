dfa39e50478d29ebbcc4b067c5c29d6d28cdf70d
Abenezer Teshome <Abmlengineer@gmail.com>
2024-06-14 22:40:00 +0000

    docker compose

--- apps/orders/src/orders.controller.ts
+++ apps/orders/src/orders.controller.ts
@@ -5,8 +5,9 @@ import { OrdersService } from './orders.service';
 export class OrdersController {
   constructor(private readonly ordersService: OrdersService) {}
 
-  @Get()
-  getHello(): string {
-    return this.ordersService.getHello();
+  @Post()
+  async reateOrder(@Body() requests : CreateOrderReques){
+    return this.orderService.cerateOrder(requests)
   }
 }
+ 
